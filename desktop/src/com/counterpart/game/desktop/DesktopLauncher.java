ad8aaad728f28979e8c5e498c9723e6522bfb0f9
Augustus Wadlington <gusbyrd@gmail.com>
2017-04-09 16:17:55 -0400

    first commit

---
+++ desktop/src/com/counterpart/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.counterpart.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.counterpart.game.CounterpartGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CounterpartGame(), config);
+	}
+}
