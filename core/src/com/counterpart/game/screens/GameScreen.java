6c69db6195e5bec8827233eec784bffed01e53db
kentjhall <kentjhall3@hotmail.com>
2017-04-09 16:58:08 -0400

    set up screen with batch and render method

--- core/src/com/counterpart/game/screens/GameScreen.java
+++ core/src/com/counterpart/game/screens/GameScreen.java
@@ -1,12 +1,20 @@
 package com.counterpart.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Created by kenthall on 4/9/17.
  */
 
 public class GameScreen implements Screen {
+    private static SpriteBatch batch;
+
+    public GameScreen (SpriteBatch batch) {
+        this.batch = batch;
+    }
 
     @Override
     public void show() {
@@ -15,7 +23,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(1, 1, 1, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
